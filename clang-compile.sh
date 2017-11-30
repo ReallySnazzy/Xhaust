1f9b586d2593d9c74d3ab1dcb9213aedf6ed8934
Samy Bencherif <samybencherif@wireless-152-13-52-53.uncg.edu>
2017-11-30 03:21:14 -0500

    .exe is ugly on posix

--- clang-compile.sh
+++ clang-compile.sh
@@ -4,5 +4,5 @@ clang++ -g -c ../src/treegen.cpp -std=c++11
 clang++ -g -c ../src/main.cpp -std=c++11
 clang++ -g -c ../src/interpreter.cpp -std=c++11
 clang++ -g -c ../src/standard_functions.cpp -std=c++11
-clang++ -g -o xhaust.exe lexer.o treegen.o interpreter.o standard_functions.o main.o -std=c++11
+clang++ -g -o xhaust lexer.o treegen.o interpreter.o standard_functions.o main.o -std=c++11
 cd ..
