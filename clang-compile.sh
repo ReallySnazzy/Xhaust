ab757c22c4d2830753bf6b2cc267808361c0eb38
Samy Bencherif <samybencherif@wireless-152-13-52-53.uncg.edu>
2017-11-30 02:44:01 -0500

    moving towards clang

---
+++ clang-compile.sh
@@ -0,0 +1,10 @@
+cd build
+clang -g -c ../src/lexer.cpp -std=c++11
+clang -g -c ../src/treegen.cpp -std=c++11
+clang -g -c ../src/main.cpp -std=c++11
+clang -g -c ../src/interpreter.cpp -std=c++11
+clang -g -c ../src/standard_functions.cpp -std=c++11
+#well I can get this far pretty damn easily..
+
+#clang -g -o xhaust.exe lexer.o treegen.o interpreter.o standard_functions.o main.o -std=c++11
+cd ..
