5610a1a5648f0a5e073bd1574f3df991dfece1f9
Samy Bencherif <samybencherif@wireless-152-13-52-53.uncg.edu>
2017-11-30 01:38:26 -0500

    error test 2

--- compile.bat
+++ compile.bat
@@ -1,3 +1,6 @@
+echo # Bro check me out this batch script is also a bash script because I am amazing > NUL
+echo # Although, tbh the hardest part was these comments > NUL
+
 cd build
 g++ -g -c ../src/lexer.cpp -std=c++11
 g++ -g -c ../src/treegen.cpp -std=c++11
@@ -5,4 +8,5 @@ g++ -g -c ../src/main.cpp -std=c++11
 g++ -g -c ../src/interpreter.cpp -std=c++11
 g++ -g -c ../src/standard_functions.cpp -std=c++11
 g++ -g -o xhaust.exe lexer.o treegen.o interpreter.o standard_functions.o main.o -std=c++11
+exit $?
 cd ..
