b951ffd8d79447724d3caede61c2aca67d5671ff
Samy Bencherif <samybencherif@wireless-152-13-52-53.uncg.edu>
2017-11-30 01:56:18 -0500

    kill crack dream of polyglot build script

--- compile.sh
+++ compile.sh
@@ -1,31 +1,10 @@
-
-# does some magic, then runs the batch file in an OSX friendly way
-
-if [ "$1" != "run" ]; then
-docker run --rm -it -v "$PWD":/usr/src/xhaust -w /usr/src/xhaust gcc:4.9 sh ./compile.bat
-mv build/xhaust.exe build/xhaust
-else
-docker run --rm -it -v "$PWD":/usr/src/xhaust -w /usr/src/xhaust gcc:4.9 ./build/xhaust $2
-fi
-
-# it's hacky, but run `./compile.sh` to compile.
-# and `.complile.sh run` to run.
-
-# FAQ
-
-# First compilation may take a long time,
-# because the gcc image needs to be downloaded
-
-# If you're running Linux
-	# You don't need this script
-	# Run `chmod +x compile.bat`
-	# Run the "batch" file with `./compile.bat`
-
-# If you're running Windows
-	# Why are you trying to run an sh script you dumbass?
-
-# Receiving "bash: docker: command not found" error
-	# You must install docker
-
-# Receiving "Cannot connect to the Docker daemon..." error
-	# Start the docker daemon
+cd build
+g++ -g -c ../src/lexer.cpp -std=c++11
+g++ -g -c ../src/treegen.cpp -std=c++11
+g++ -g -c ../src/main.cpp -std=c++11
+g++ -g -c ../src/interpreter.cpp -std=c++11
+g++ -g -c ../src/standard_functions.cpp -std=c++11
+g++ -g -o xhaust lexer.o treegen.o interpreter.o standard_functions.o main.o -std=c++11
+response=$?
+cd ..
+exit response
