f83d10a62a221642e1a7d939aaaa72d0fe646f1a
Samy Bencherif <samybencherif@wireless-152-13-52-53.uncg.edu>
2017-11-30 01:04:54 -0500

    be patriotic

--- compile.bat
+++ compile.bat
@@ -4,5 +4,5 @@ g++ -g -c ../src/treegen.cpp -std=c++11
 g++ -g -c ../src/main.cpp -std=c++11
 g++ -g -c ../src/interpreter.cpp -std=c++11
 g++ -g -c ../src/standard_functions.cpp -std=c++11
-g++ -g -o lang.exe lexer.o treegen.o interpreter.o standard_functions.o main.o -std=c++11
+g++ -g -o xhaust.exe lexer.o treegen.o interpreter.o standard_functions.o main.o -std=c++11
 cd ..
