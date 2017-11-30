7257a86692ff19cd0a44aa58a91cedc790647752
Samy Bencherif <samybencherif@wireless-152-13-52-53.uncg.edu>
2017-11-30 01:51:15 -0500

    just a .bat now

--- compile.bat
+++ compile.bat
@@ -1,6 +1,3 @@
-echo # Bro check me out this batch script is also a bash script because I am amazing > NUL
-echo # Although, tbh the hardest part was these comments > NUL
-
 cd build
 g++ -g -c ../src/lexer.cpp -std=c++11
 g++ -g -c ../src/treegen.cpp -std=c++11
@@ -9,4 +6,3 @@ g++ -g -c ../src/interpreter.cpp -std=c++11
 g++ -g -c ../src/standard_functions.cpp -std=c++11
 g++ -g -o xhaust.exe lexer.o treegen.o interpreter.o standard_functions.o main.o -std=c++11
 cd ..
-exit $?
